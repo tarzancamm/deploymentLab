8dbafda4e81ad49089d84fdfb5c54d827a7a317c
Cameron Miner <cameronsminer@gmail.com>
2022-11-02 14:40:34 -0600

    Setup files structure

---
+++ server.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+const { SERVER_PORT } = process.env
+
+const app = express()
+
+// Middleware
+app.use(express.json())
+
+
+const {home} = require('./controller')
+app.get('/', home)
+
+app.listen(SERVER_PORT, () => console.log(`Server running on port ${SERVER_PORT}`))
